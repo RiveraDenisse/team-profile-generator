be773096a061cc40076ac02854aa996dbd1a35fa
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-15 23:22:08 -0500

    added getName test to employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,20 @@
 const Employee = require("../lib/Employee");
 
 
-test('assign name to new employee', () => {
-    const employee = new Employee('Denisse');
-
-    expect(employee.name).toBe('Denisse');
-    //expect(employee.value).toEqual(expect.any(Number));
+//test creates a new employee with name,id,email
+test('creates an employee with name id and email', () => {
+    const employee = new Employee("Denisse",20,"test@email","Employee");
+    //checks if the name is a string
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.role).toEqual('Employee');
 });
 
-
-test('assign name to new employee', () => {
-    const employee = new Employee('Denisse');
-
-    expect(employee.name).toBe('Denisse');
+//getName()
+test("get employee name", () => {
+    const employee = new Employee("Denisse",20,"test@email","Employee");
     
-});
+    expect(employee.getName()).toBe("Denisse");
+})
+
