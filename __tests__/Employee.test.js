27eea8dd1c9629fd6bc9d565be1ec28a2b34d2d6
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-16 23:58:02 -0500

    completer Manager testing as well as modified functions to class constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,10 +5,10 @@ const employee = new Employee("Denisse",20,"test@email.com","Employee");
 //test creates a new employee with name,id,email
 test('creates an employee with name id and email', () => {
     //checks if the name is a string
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
-    expect(employee.role).toEqual('Employee');
+    expect(employee.name).toBe("Denisse");
+    expect(employee.id).toBe(20);
+    expect(employee.email).toBe("test@email.com");
+    expect(employee.role).toBe('Employee');
 });
 
 //getName()
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,23 @@
 const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+//to create new employee class
+const manager = new Manager("Denisse",20,"manager@email.com","Manager","123-456-7890");
 
+//test creates a new employee with name,id,email
+test('creates a new Manager', () => {
+    //checks if the name is a string
+    expect(manager.name).toBe("Denisse");
+    expect(manager.id).toBe(20);
+    expect(manager.email).toBe("manager@email.com");
+    expect(manager.role).toBe("Manager");
+    expect(manager.phone).toEqual(expect.any(String));
+});
+
+//getrole()
+test("get manager role", () => {
+    expect(manager.getRole()).toBe("Manager");
+})
+//getphone()
+test("get manager phone", () => {
+    expect(manager.getPhone()).toEqual(expect.any(String));
+})
