e17d8e99bac8d1bde0d12a4e51f1f6f16e66efc1
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-16 22:18:48 -0500

    completed employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
 const Employee = require("../lib/Employee");
-
+//to create new employee
+const employee = new Employee("Denisse",20,"test@email.com","Employee");
 
 //test creates a new employee with name,id,email
 test('creates an employee with name id and email', () => {
-    const employee = new Employee("Denisse",20,"test@email","Employee");
     //checks if the name is a string
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -12,9 +12,21 @@ test('creates an employee with name id and email', () => {
 });
 
 //getName()
-test("get employee name", () => {
-    const employee = new Employee("Denisse",20,"test@email","Employee");
-    
+test("get employee name", () => {    
     expect(employee.getName()).toBe("Denisse");
+})
+
+//getId()
+test("get employee ID", () => {
+    expect(employee.getId()).toBe(20);
+})
+
+//getemail()
+test("get employee email", () => {
+    expect(employee.getEmail()).toBe("test@email.com");
+})
+//getrole()
+test("get employee role", () => {
+    expect(employee.getRole()).toBe("Employee");
 })
 
