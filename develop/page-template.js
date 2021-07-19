738b44af1474f90cf187927dd1a36103678b3ab9
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-07-18 20:52:55 -0500

    created cards for manager fixed get function for employee and engineer

--- develop/page-template.js
+++ develop/page-template.js
@@ -1,5 +1,43 @@
+//team card
+function teamCard(data){
+    return`
+    <div class="row row-cols-1 row-cols-sm-3 g-4">
+<div class="col-4">
+    <div class="card mb-5 border-dark">
+    <div class="card-body bg-secondary">
+        <h5 class="card-title">${data.getName()}</h5>
+        <p class="card-text">${data.getRole()}</p>
+    </div>
+    <div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item card-link">ID: ${data.getId()}</li>
+            <li class="list-group-item">Email: ${data.getEmail()}</li>
+            <li class="list-group-item">Office number: ${data.getOffice()}</li>
+        </ul>
+    </div>
+    <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+    </div>
+</div>
+</div>  `
+};
 //template for HTML file
-function generatePage(data) {
+const generatePage = data => {
+    console.log('hello=========================================')
+    console.log(data);
+    let cards = "";
+    for (let i =0; i<data.length; i++) {
+        console.log(data[i]);
+        if (data[i].getRole() === 'Manager'){
+            console.log("I am manager");
+            cards= cards+teamCard(data[i]);
+        }else if (data[i].getRole() === 'Engineer'){
+            console.log("I am engineer");
+            cards= cards+EngineerCard(data[i]);
+        }
+    } 
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -26,14 +64,14 @@ function generatePage(data) {
                 <div class="col-4">
                     <div class="card mb-5 border-dark">
                     <div class="card-body bg-secondary">
-                        <h5 class="card-title">${data.managerName}</h5>
-                        <p class="card-text">${data.managerRole}</p>
+                        <h5 class="card-title">data.getName</h5>
+                        <p class="card-text">Manager</p>
                     </div>
                     <div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item card-link">ID: ${data.managerId}</li>
-                            <li class="list-group-item">Email: ${data.managerEmail}</li>
-                            <li class="list-group-item">Office number: ${data.managerNumber}</li>
+                            <li class="list-group-item card-link">ID: data.getId</li>
+                            <li class="list-group-item">Email: data.getEmail</li>
+                            <li class="list-group-item">Office number: data.getOffice()</li>
                         </ul>
                     </div>
                     <div class="card-body">
@@ -43,6 +81,7 @@ function generatePage(data) {
                     </div>
                 </div>
             </div>  
+            ${cards}
         </div>
     </body>
     </html>
--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./develop/page-template');
+//const Employee = require(`./lib/Employee`);
+const Engineer = require(`./lib/Engineer`);
+const Manager = require(`./lib/Manager`);
+const Intern = require(`./lib/Intern`);
+const teamMembers = [];
 
 //TODO: create manager's questions
 const managerquestions = () => {
@@ -58,22 +63,59 @@ const managerquestions = () => {
           }
         }
     }
-]);
+  ])
+  .then (memberData =>{
+    memberData.role === 'Manager';
+    teamMembers.push(new Manager(memberData.managerName,memberData.managerId,memberData.managerEmail,memberData.managerNumber,memberData.role));
+        console.log(teamMembers);
+  })
 };
-const createTeam = teamData => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'License',
-            message: "Please choose a license to your project from the list below",
-            choices: ['MIT', 'Apache2.0', 'GNUGPLv3', 'MozillaPublicLicense2.0', 'TheUnlicense']
+const AddTeam = () => {
+  return inquirer.prompt([
+      {
+          type: 'list',
+          name: 'options',
+          message: "What would you like to do?",
+          choices: ['Add an Engineer', 'Add an Intern', 'Finish']
+          //need to validate if engineer,intern or finish
+      }, 
+      {
+        type: 'input',
+        name: 'Name',
+        message: "Please enter employee's name (Required)",
+        validate: NameInput => {
+          if (NameInput) {
+            return true;
+          } else {
+            console.log("Employee's name is required");
+            return false;
           }
-    ]);
-    
+        }
+      }
+    ])
+    //end of questions
+    .then (memberData => {
+      if (memberData.options === 'Add an Engineer') {
+        memberData.role = 'Engineer';
+        memberData.id = '123';
+        memberData.email = 'email@';
+        memberData.github = 'Github';
+        teamMembers.push(new Engineer(memberData.Name,memberData.id,memberData.email,memberData.role,memberData.github));
+        console.log(teamMembers);
+        AddTeam();
+        //promptEngineer();
+        } //else if (memberData.options === 'Add an Intern'){
+           //AddTeam();
+        //} 
+        else {
+            return teamMembers;
+        }
+  })
 };
+
 //TODO: Create a function to write HTML file
-function writeToFile(fileName,data){
-    fs.writeFile(fileName,data,err =>{
+function writeToFile(fileName,teamMembers){
+    fs.writeFile(fileName,teamMembers,err =>{
         if (err){
             return console.log(err);
         }
@@ -82,10 +124,7 @@ function writeToFile(fileName,data){
 }
 //Create a function to initialize app starting with manager questions
 managerquestions()
-    //give option to create engineer,intern or finish
-   
-     //then userfeedback will be used to create HTML
-    .then((data)=>writeToFile('TeamRoster.html',generatePage(data)))
-
-    
-
+  //give option to create engineer,intern or finish
+  .then(AddTeam)
+  //then userfeedback will be used to create HTML
+  .then(()=>writeToFile('TeamRoster.html',generatePage(teamMembers)))
